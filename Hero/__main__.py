3171cf49bbea762ee9724941c60d9105d126065a
/Oꜰꜰʟɪɴᴇ ‌[ᴀꜰᴋ]「™ʟᴏɢ✓ᴏᴜᴛ❥︎✔︎ <94526115+Unknownvip@users.noreply.github.com>
2022-02-27 01:19:05 +0530

    Update __main__.py

--- Hero/__main__.py
+++ Hero/__main__.py
@@ -236,13 +236,13 @@ home_text_pm = f"""ʜᴇʟʟᴏ ,
 ᴀʟʟ  ᴄᴏᴍᴍᴀɴᴅs ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ: / """
 
 
-@app.on_message(filters.command("help") & filters.private)
+@app.on_message(filters.command("null") & filters.private)
 async def help_command(_, message):
     text, keyboard = await help_parser(message.from_user.mention)
     await app.send_message(message.chat.id, text, reply_markup=keyboard)
 
 
-@app.on_message(filters.command("hero") & filters.private)
+@app.on_message(filters.command("vip") & filters.private)
 async def start_command(_, message):
     if len(message.text.split()) > 1:
         name = (message.text.split(None, 1)[1]).lower()
@@ -289,7 +289,7 @@ async def start_command(_, message):
                     LOG_GROUP_ID,
                     f"{message.from_user.mention} ʜᴀs ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <code>sᴜᴅᴏʟɪsᴛ</code>\n\n**ᴜsᴇʀ ɪᴅ:** {sender_id}\n**ᴜsᴇʀ ɴᴀᴍᴇ:** {sender_name}",
                 )
-        if name == "help":
+        if name == "null":
             text, keyboard = await help_parser(message.from_user.mention)
             await message.delete()
             return await app.send_text(
@@ -390,7 +390,7 @@ async def shikhar(_, CallbackQuery):
     await CallbackQuery.message.edit(text, reply_markup=keyboard)
 
 
-@app.on_callback_query(filters.regex(r"elp_(.*?)"))
+@app.on_callback_query(filters.regex(r"vip_(.*?)"))
 async def help_button(client, query):
     home_match = re.match(r"help_home\((.+?)\)", query.data)
     mod_match = re.match(r"help_module\((.+?)\)", query.data)
